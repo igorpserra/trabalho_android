d59906bcf03597eb4d63834fca8ef54010eae645
igorpserra <igorpserra@gmail.com>
2018-05-24 00:26:15 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/tarefasapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.tarefasapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** resposta ao botao  */
+    fun sendMessage(view: View) {
+        // Do something in response to button
+    }
+}
